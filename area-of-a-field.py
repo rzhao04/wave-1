751493f1cc82be89277136cc065d71c0b79a0e91
richard <richardjzzhao@gmail.com>
2020-04-11 21:32:37 -0400

    upload to wave-1

---
+++ area-of-a-field.py
@@ -0,0 +1,9 @@
+length = float
+length = input ('what is length: ')    
+width = float
+width = input ('what is width: ')
+area = float
+area = length * width
+areaInAcres = float
+areaInAcres = area / 43560
+print (areaInAcres)
