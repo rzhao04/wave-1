7d54505349879ac1385f0f12a3240dba2fc6bce4
richard <richardjzzhao@gmail.com>
2020-04-13 17:21:04 -0400

    upload third assignment to wave-1

---
+++ compound-interest.py
@@ -0,0 +1,9 @@
+money = float
+money = input ('what is money: ')
+interest = 1.04
+savingsYear1 = money * interest
+savingsYear2 = savingsYear1 * interest
+savingsYear3 = savingsYear2 * interest
+print (round (savingsYear1, 2))
+print (round (savingsYear2, 2))
+print (round (savingsYear3, 2))
