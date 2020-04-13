13c809d4fc864f01f7babf42d5e6936b64132b39
richard <richardjzzhao@gmail.com>
2020-04-13 17:03:12 -0400

    upload sec assignment to wave-1

---
+++ volume-of-a-cylinder.py
@@ -0,0 +1,8 @@
+radius = float
+radius = input ('what is radius: ')
+height = float
+height = input ('what is height: ')
+pi = 3.14
+volume = float
+volume = pi * radius * radius * height
+print (round (volume, 1))
